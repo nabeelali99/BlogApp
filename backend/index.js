79de3f8dea3e49465d25c7c6fd3047347852b2e3
Nabeel Ali <nabeelhell@gmail.com>
2024-05-31 22:00:21 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: true,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
