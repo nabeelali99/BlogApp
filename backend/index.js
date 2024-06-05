2bf7a695638827e482331e81b5151877547a916d
Nabeel Ali <nabeelhell@gmail.com>
2024-06-05 22:00:33 +0530

    Fix

--- backend/index.js
+++ backend/index.js
@@ -34,11 +34,7 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization",
-    "Access-Control-Allow-Origin"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
