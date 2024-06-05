89cebafc55705927d3f47c38e5355179964c02f2
Nabeel Ali <nabeelhell@gmail.com>
2024-06-05 19:45:51 +0530

    Fix Cors

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,14 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://bloggerz-blogapp-frontend.onrender.com",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
