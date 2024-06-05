6396b6490974e745f8cec31aec8b04105885b5a4
Nabeel Ali <nabeelhell@gmail.com>
2024-06-05 19:57:09 +0530

    Fix Cors

--- backend/index.js
+++ backend/index.js
@@ -27,11 +27,24 @@ app.use(express.json());
 
 // app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://bloggerz-blogapp-frontend.onrender.com"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(
   cors({
     origin: "https://bloggerz-blogapp-frontend.onrender.com",
     credentials: true,
     optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
